d7a66d7e8ddce198c06f78fc0b7163208fc539eb
Martin Pretorius <glasnoster@gmail.com>
2018-01-30 20:58:42 +0200

    Add Root example code

--- app/services/facebook_service.rb
+++ app/services/facebook_service.rb
@@ -1,15 +1,36 @@
 class FacebookService
   class << self
     def handle_message(sender_id, message)
-      if message['text']
-        echo_message(sender_id, message)
+      if text = message['text']
+        reply = case text
+        when /list all phone brands/
+          phone_brands.join("\n")
+        when /list all the phones made by (.*)/
+          brand = $1
+          phones = phones_by_brand(brand)
+          if phones.count > 0
+            phones.join("\n")
+          else
+            "Therea are no phones made by #{brand}"
+          end
+        when /what is the value of a (.*)/
+          phone = $1
+          matches = phone_matches(phone)
+
+          matches.map { |phone| "#{phone['name']}: *R#{phone['value'].to_f/100}*" }
+            .join("\n")
+        else
+          "I don't understand"
+        end
+
+        send_message(sender_id, reply) if reply
       end
     end
 
-    def echo_message(receiver_id, message)
+    def send_message(receiver_id, message)
       data = {
         recipient: {id: receiver_id},
-        message:   {text: message['text']}
+        message:   {text: message}
       }
 
       HTTParty.post("https://graph.facebook.com/v2.6/me/messages",
@@ -21,6 +42,27 @@ class FacebookService
     private
     def page_access_token
       Settings.facebook.page_access_token
+    end
+
+    def phone_brands
+      root_client.list_gadget_models.map{ |phone| phone['make'] }.uniq
+    end
+
+    def phones_by_brand(brand)
+      brand_lowercase = brand.downcase
+      root_client.list_gadget_models
+        .select { |phone| phone['make'].downcase == brand_lowercase }
+        .map{ |phone| phone['name'] }
+    end
+
+    def phone_matches(phone)
+      phone_lowercase = phone.downcase
+      root_client.list_gadget_models
+        .select { |phone| phone['name'].downcase.include?(phone_lowercase) }
+    end
+
+    def root_client
+      Root::Insurance::Client.new(Settings.root.app_id, Settings.root.app_secret)
     end
   end
 end
