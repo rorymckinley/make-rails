07d0102bf1adda3caf0852b39db7fbb86b08a411
Rory McKinley <rorymckinley@gmail.com>
2018-02-07 10:57:49 +0200

    Crude prototype

--- app/services/facebook_service.rb
+++ app/services/facebook_service.rb
@@ -1,7 +1,10 @@
 class FacebookService
   class << self
     def handle_message(sender_id, message)
-      if message['text']
+      if message['text'] and message['text'] =~ /Show me (.+)!/
+        gadgets = fetch_root_gadgets($1)
+        transmit(sender_id, gadgets.join("\n"))
+      elsif message['text']
         echo_message(sender_id, message)
       end
     end
@@ -21,6 +24,28 @@ class FacebookService
     private
     def page_access_token
       Settings.facebook.page_access_token
+    end
+
+    def fetch_root_gadgets(make)
+      p root_client.list_gadget_models
+      products = root_client.list_gadget_models.select { |data| data["make"] == make }
+      products.map { |data| data["name"] }
+    end
+
+    def root_client
+      Root::Insurance::Client.new(Settings.root.app_id, Settings.root.app_secret)
+    end
+
+    def transmit(receiver_id, message)
+      data = {
+        recipient: {id: receiver_id},
+        message:   {text: message}
+      }
+
+      HTTParty.post("https://graph.facebook.com/v2.6/me/messages",
+        body:    data.to_json,
+        query:   {access_token: page_access_token},
+        headers: {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
     end
   end
 end
